1eeda73d8cd10a351670ee193d87486de6d5af4f
pdrmend <pdrmend@gmail.com>
2015-05-20 14:37:02 +0100

    Update cachematrix.R
    Pedro's solution to cache inverse of matrix assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+#####################################################
+#                       
+#
+#         Cache Inverse of Matrix
+#
+####################################################
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#
+# This function creates a special matrix that is able to cache its inverse by 
+# storing a list of functions: set, get, setinverse, getinverse.
+# These functions can be called individually by name_of_special_matrix + $ + 
+# name_of_function.
 
+makeCacheMatrix <- function(x = numeric()) {
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inmatrix) im <<- inmatrix
+        getinverse <- function() im
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+
+#
+#You can use this matrix to run as argument to makeCacheMatrix():
+# samp <- sample(100,16)
+# mat <- matrix(samp, 4,4)
+# 
+
+
+#
+# This function checks if the special matrix created by the above function
+# has a cached inverse. If yes it returns a message stating that it does and 
+# and the inverted matrix.
+# If not it computes the inverse, caches it in the special matrix, and returns it.
+#
+#
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getinverse()
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)
+        x$setinverse(im)
+        im
 }
